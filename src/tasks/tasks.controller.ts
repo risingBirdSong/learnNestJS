5fa3eafcafdf1f41993b57c09e6a76e00934ea8f
risingbirdsong <heregoingthere@outlook.com>
2020-03-21 16:01:50 -0700

    succesfully creating a task from a POST request

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
+enum taskParams {
+  title = "title",
+  description = "description"
+}
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) { }
+  @Get()
+  getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+  @Post()
+  createTask(
+    @Body(taskParams.description) description: string,
+    @Body(taskParams.title) title: string
+  ) {
+    return this.taskService.createTask(title, description);
+  }
+
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { TaskI } from './task.model';
+import { TaskStatus } from "./task.model";
+import { v4 as uuidv4 } from 'uuid';
+
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: TaskI[] = [];
+
+  getAllTasks(): TaskI[] {
+    return this.tasks;
+  }
+  createTask(title: string, description: string): TaskI {
+    let task: TaskI = {
+      title,
+      description,
+      status: TaskStatus.OPEN,
+      id: uuidv4()
+    }
+    this.tasks.push(task);
+    return task;
+  }
+}
