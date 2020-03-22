b8ac34927d687a75fa3aa5ed3fc55e72e8313df9
risingbirdsong <heregoingthere@outlook.com>
2020-03-21 19:02:25 -0700

    implemented dto and also delete task routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { TaskI } from './task.model';
 
-enum taskParams {
-  title = "title",
-  description = "description"
+enum routeParams {
+  urlParam_id = '/:id',
+  id = 'id'
 }
+
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) { }
@@ -12,12 +15,21 @@ export class TasksController {
   getAllTasks() {
     return this.taskService.getAllTasks();
   }
+  @Get(routeParams.urlParam_id)
+  getSingleTask(@Param(routeParams.id) id: string): TaskI {
+    return this.taskService.getTaskById(id);
+  }
   @Post()
   createTask(
-    @Body(taskParams.description) description: string,
-    @Body(taskParams.title) title: string
+    @Body() createTaskDto: CreateTaskDto,
   ) {
-    return this.taskService.createTask(title, description);
+    return this.taskService.createTask(createTaskDto);
+  }
+  @Delete(routeParams.urlParam_id)
+  deleteTaskByTheId(@Param(routeParams.id) id: string): void {
+    let deleted = this.taskService.deleteTask(id);
+    console.log("the following task was delete", deleted);
+    console.log("the array after deletion", this.taskService.getAllTasks())
   }
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,16 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { TaskI } from './task.model';
 import { TaskStatus } from "./task.model";
 import { v4 as uuidv4 } from 'uuid';
-
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-  private tasks: TaskI[] = [];
+  private tasks: TaskI[] = [
+    {
+      "title": "another title",
+      "description": "another desc",
+      status: TaskStatus.OPEN,
+      "id": "e000082e-4099-4546-acdb-a7f9a1024d73"
+    },
+    {
+      "title": "title aaa",
+      "description": "desc aaa",
+      "status": TaskStatus.OPEN,
+      "id": "715f5508-b02b-4566-bdee-203818a2934b"
+    }
+  ];
 
   getAllTasks(): TaskI[] {
     return this.tasks;
   }
-  createTask(title: string, description: string): TaskI {
+
+  getTaskById(id: string): TaskI {
+    return this.tasks.find(tsk => tsk.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): TaskI {
+    const { title, description } = createTaskDto;
     let task: TaskI = {
       title,
       description,
@@ -20,5 +39,10 @@ export class TasksService {
     }
     this.tasks.push(task);
     return task;
+  }
+  deleteTask(id: string): TaskI {
+    let taskToBeDeleated = this.tasks.find(task => task.id === id);
+    this.tasks = this.tasks.filter(task => task.id !== id);
+    return taskToBeDeleated;
   }
 }
